3580f16ad6428810e62c868a1dbd69f7473ecf53
Pasquale <61435712+Psq333@users.noreply.github.com>
2024-02-22 11:30:04 +0100

    Delete SnowRace Microservizi/Sviluppo Microservizi directory

--- SnowRace Microservizi/Sviluppo Microservizi/src/main/java/it/contrader/impiantoservice/exceptions/NotExistAdminException.java
+++
@@ -1,7 +0,0 @@
-package it.contrader.impiantoservice.exceptions;
-
-public class NotExistAdminException extends RuntimeException{
-    public NotExistAdminException(String message){
-        super(message);
-    }
-}
